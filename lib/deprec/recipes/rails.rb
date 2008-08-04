4d3093b5498afc176edd3cdbe92fb8c5148d0756
Brendan Schwartz <brendanschwartz@gmail.com>
2008-08-04 09:11:52 -0400

    Slimmed down the generate_database_yml. We don't need different variables for every possible stage!

--- lib/deprec/recipes/rails.rb
+++ lib/deprec/recipes/rails.rb
@@ -204,40 +204,24 @@ Capistrano::Configuration.instance(:must_exist).load do
       #
       # if (self.respond_to?("db_host_#{rails_env}".to_sym)) # doesn't seem to work
 
-      set :db_host_default, lambda { Capistrano::CLI.prompt 'Enter database host', 'localhost'}
-      set :db_host_staging, lambda { db_host_default }
-      set :db_host_production, lambda { db_host_default }
-
-      set :db_name_default, lambda { Capistrano::CLI.prompt 'Enter database name', "#{application}_#{rails_env}" }
-      set :db_name_staging, lambda { db_name_default }
-      set :db_name_production, lambda { db_name_default }
-
-      set :db_user_default, lambda { Capistrano::CLI.prompt 'Enter database user', 'root' }
-      set :db_user_staging, lambda { db_user_default }
-      set :db_user_production, lambda { db_user_default }
-
-      set :db_pass_default, lambda { Capistrano::CLI.prompt 'Enter database pass', '' }
-      set :db_pass_staging, lambda { db_pass_default }
-      set :db_pass_production, lambda { db_pass_default }
-
-      set :db_adaptor_default, lambda { Capistrano::CLI.prompt 'Enter database adaptor', 'mysql' }
-      set :db_adaptor_staging, lambda { db_adaptor_default }
-      set :db_adaptor_production, lambda { db_adaptor_default }
-
-      set :db_socket_default, lambda { Capistrano::CLI.prompt('Enter database socket', '')}
-      set :db_socket_staging, lambda { db_socket_default }
-      set :db_socket_production, lambda { db_socket_default }
+      set :db_host, 'localhost'
+      set :db_name, lambda { "#{application}_#{rails_env}" }
+      set :db_user, lambda { application }
+      set :db_pass, lambda { application } # password is the same as the user
+      set :db_adaptor, 'mysql'
+      set :db_socket, nil
 
       task :generate_database_yml, :roles => :app do    
-        database_configuration = render :template => <<-EOF
-        #{rails_env}:
-        adapter: #{self.send("db_adaptor_#{rails_env}")}
-        database: #{self.send("db_name_#{rails_env}")}
-        username: #{self.send("db_user_#{rails_env}")}
-        password: #{self.send("db_pass_#{rails_env}")}
-        host: #{self.send("db_host_#{rails_env}")}
-        socket: #{self.send("db_socket_#{rails_env}")}
-        EOF
+        database_configuration = <<-EOF
+#{rails_env}:
+  adapter: #{db_adaptor}
+  database: #{db_name}
+  username: #{db_user}
+  password: #{db_pass}
+  host: #{db_host}
+EOF
+        database_configuration << "socket: #{db_socket}" if db_socket
+        
         run "mkdir -p #{deploy_to}/#{shared_dir}/config" 
         put database_configuration, "#{deploy_to}/#{shared_dir}/config/database.yml" 
       end
